fe9c08efcc3bcadebbf77566b13f4752f0116efa
Zander5357 <bagasgnwn@gmail.com>
2023-11-26 16:44:33 +0700

    Throughout heaven and Earth, I alone am the honored one.

--- src/Logic/Order.java
+++ src/Logic/Order.java
@@ -1,33 +1,82 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Logic;
 
-/**
- *
- * @author 62811
- */
 public class Order {
-    private String itemType;
-    private String washingType;
-    private double weight;
-    private int totalItem;
-    private int duration;
-    
-    public Order(String itemType, String washingType, double weight, int totalItem, int duration){
-        this.itemType = itemType;
-        this.washingType = washingType;
-        this.weight = weight;
-        this.totalItem = totalItem;
-        this.duration = duration;
+
+    private String namaPelanggan;
+    private String nomorTeleponPelanggan;
+    private String jenisBarang;
+    private String jenisCuci;
+    private double beratBarang;
+    private int jumlahBarang;
+    private int durasiCuci;
+
+    public Order(String jenisBarang, String jenisCuci, double beratBarang, int durasiCuci) {
+        this.jenisBarang = jenisBarang;
+        this.jenisCuci = jenisCuci;
+        this.beratBarang = beratBarang;
+        this.durasiCuci = durasiCuci;
     }
-    
-    public Order(String itemType, int totalItem, int duration){
-        this.itemType = itemType;
-        this.totalItem = totalItem;
-        this.duration = duration;
+
+    public Order(String jenisBarang, int jumlahBarang, int durasiCuci) {
+        this.jenisBarang = jenisBarang;
+        this.jumlahBarang = jumlahBarang;
+        this.durasiCuci = durasiCuci;
     }
-    
-    
+
+    public String getNamaPelanggan() {
+        return namaPelanggan;
+    }
+
+    public String getNomorTeleponPelanggan() {
+        return nomorTeleponPelanggan;
+    }
+
+    public String getJenisBarang() {
+        return jenisBarang;
+    }
+
+    public String getJenisCuci() {
+        return jenisCuci;
+    }
+
+    public double getBeratBarang() {
+        return beratBarang;
+    }
+
+    public int getJumlahBarang() {
+        return jumlahBarang;
+    }
+
+    public int getDurasiCuci() {
+        return durasiCuci;
+    }
+
+    public void setNamaPelanggan(String namaPelanggan) {
+        this.namaPelanggan = namaPelanggan;
+    }
+
+    public void setNomorTeleponPelanggan(String nomorTeleponPelanggan) {
+        this.nomorTeleponPelanggan = nomorTeleponPelanggan;
+    }
+
+    public void setJenisBarang(String jenisBarang) {
+        this.jenisBarang = jenisBarang;
+    }
+
+    public void setJenisCuci(String jenisCuci) {
+        this.jenisCuci = jenisCuci;
+    }
+
+    public void setBeratBarang(double beratBarang) {
+        this.beratBarang = beratBarang;
+    }
+
+    public void setJumlahBarang(int jumlahBarang) {
+        this.jumlahBarang = jumlahBarang;
+    }
+
+    public void setDurasiCuci(int durasiCuci) {
+        this.durasiCuci = durasiCuci;
+    }
+
 }
